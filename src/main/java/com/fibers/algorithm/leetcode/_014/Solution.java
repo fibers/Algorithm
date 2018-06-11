971abd349db6487bcd45bb4dbb5a71f89ed137ec
fibers <yu8582@gmail.com>
2018-06-11 10:26:48 +0800

    update

--- src/main/java/com/fibers/algorithm/leetcode/_014/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_014/Solution.java
@@ -1,4 +1,13 @@
 package com.fibers.algorithm.leetcode._014;
 
 public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+
+
+
+
+
+        return result;
+    }
 }
