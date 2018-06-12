fba1fcd9c50156da54bfbe7224c84984125aaeef
fibers <yu8582@gmail.com>
2018-06-12 19:50:02 +0800

    update

--- src/main/java/com/fibers/algorithm/leetcode/_101/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_101/Solution.java
@@ -5,7 +5,20 @@ import com.fibers.algorithm.datastructure.TreeNode;
 public class Solution {
 
     public boolean isSymmetric(TreeNode root) {
-
+        return isMirror(root, root);
     }
-    
+
+    private boolean isMirror(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) {
+            return true;
+        }
+
+        if (n1 == null || n2 == null) {
+            return false;
+        }
+
+        return (n1.val == n2.val)
+                && isMirror(n1.left, n2.right)
+                && isMirror(n1.right, n2.left);
+    }
 }
