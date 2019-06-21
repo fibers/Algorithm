a9a433f550d4cebccd202e8fa7382f34dc69661e
fibers <yu8582@126.com>
2019-06-21 17:51:13 +0800

    Roughly commit

--- src/main/java/com/fibers/algorithm/leetcode/_172/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_172/Solution.java
@@ -1,4 +1,8 @@
 package com.fibers.algorithm.leetcode._172;
+
 public class Solution {
+    public int trailingZeroes(int n) {
+        return n <= 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
 }
 
