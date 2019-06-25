6a0964f2e944cd3fb5be752b6cdd3a9d91e465fb
fibers <yu8582@126.com>
2019-06-25 19:59:28 +0800

    Roughly commit

--- src/main/java/com/fibers/algorithm/leetcode/_016/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_016/Solution.java
@@ -1,4 +1,36 @@
 package com.fibers.algorithm.leetcode._016;
+
+import java.util.Arrays;
+
 public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+
+                if (sum > target) {
+                    end--;
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    return sum;
+                }
+            }
+        }
+        return result;
+    }
 }
 
