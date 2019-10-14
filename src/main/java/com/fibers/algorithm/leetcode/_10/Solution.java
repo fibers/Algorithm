56781802dde5047769b79f22874653f930782c2c
fibers <yu8582@126.com>
2019-10-14 15:39:46 +0800

    #10 done

--- src/main/java/com/fibers/algorithm/leetcode/_10/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_10/Solution.java
@@ -1,16 +1,16 @@
 package com.fibers.algorithm.leetcode._10;
+
 /*
  * @lc app=leetcode.cn id=10 lang=java
  *
  * [10] 正则表达式匹配
  */
-
 // @lc code=start
 class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.isMatch("aa", "a*");
+        System.out.println(s.isMatch("ab", ".*"));
     }
 
     public boolean isMatch(String s, String p) {
@@ -24,25 +24,23 @@ class Solution {
         boolean dp[][] = new boolean[sLen + 1][pLen + 1];
         dp[0][0] = true;
 
-        if (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) {
-            dp[1][1] = true;
+        for (int j = 2; j <= pLen; j++) {
+            dp[0][j] = dp[0][j - 2] && p.charAt(j - 1) == '*';
         }
 
         for (int i = 1; i <= sLen; i++) {
             for (int j = 1; j <= pLen; j++) {
-                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
                     dp[i][j] = dp[i - 1][j - 1];
-                } else if (p.charAt(j) == '*') {
-                    // abcc* match acb, as c* count as empty
-                    if (p.charAt(j - 1) != s.charAt(i)) {
+                } else if (p.charAt(j - 1) == '*') {
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    } else if (p.charAt(j - 2) != s.charAt(i - 1)) {
                         dp[i][j] = dp[i][j - 2];
-                    } else if (p.charAt(j - 1) == s.charAt(i) || p.charAt(j - 1) == '.') {
-                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                     }
                 }
             }
         }
-
         return dp[sLen][pLen];
     }
 }
