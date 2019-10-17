336103119e47eb6a645701eab6510c5f2e0f22bc
fibers <yu8582@126.com>
2019-10-17 16:39:19 +0800

    update

--- src/main/java/com/fibers/algorithm/leetcode/_18/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_18/Solution.java
@@ -1,7 +1,9 @@
 package com.fibers.algorithm.leetcode._18;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import com.fibers.utils.Utils;
 
 /*
  * @lc app=leetcode.cn id=18 lang=java
@@ -11,13 +13,47 @@ import java.util.List;
 
 // @lc code=start
 class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        // Utils.printNestList(s.fourSum(new int[] {1, 0, -1, 0, -2, 2}, 0));
+    }
+
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 4) {
             return results;
         }
 
+        Arrays.sort(nums);
 
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    if ((left != j + 1 && nums[left] == nums[left - 1])
+                            || nums[i] + nums[j] + nums[left] + nums[right] < target) {
+                        left++;
+                    } else if ((right != nums.length - 1 && nums[right] == nums[right + 1])
+                            || nums[i] + nums[j] + nums[left] + nums[right] > target) {
+                        right--;
+                    } else {
+                        results.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        // 满足条件的，进入下一次遍历
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
         return results;
     }
 }
--- src/main/java/com/fibers/algorithm/leetcode/_29/Solution.java
+++ src/main/java/com/fibers/algorithm/leetcode/_29/Solution.java
@@ -1,24 +1,41 @@
 package com.fibers.algorithm.leetcode._29;
 
+
+/*
+ * @lc app=leetcode.cn id=29 lang=java
+ *
+ * [29] 两数相除
+ */
+// @lc code=start
 public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.divide(-1, 1));
+        System.out.println(s.divide(-2147483648, 1));
     }
 
     public int divide(int dividend, int divisor) {
-        boolean sign = (dividend > 0) ^ (divisor < 0);
-
-        dividend = dividend > 0 ? dividend : 0 - dividend;
-        divisor = divisor > 0 ? divisor : 0 - divisor;
-
-        int result = 0;
-        while (dividend >= divisor) {
-            dividend -= divisor;
-            result++;
+        if (dividend == 0) {
+            return 0;
         }
 
-        return sign ? result : 0 - result;
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        boolean sign = (dividend ^ divisor) < 0;
+
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+
+        int result = 0;
+        for (int i = 31; i >= 0; i--) {
+            if ((absDividend >> i) >= absDivisor) {
+                result += 1 << i;
+                absDividend -= absDivisor << i;
+            }
+        }
+        return sign ? 0 - result : result;
     }
 }
+// @lc code=end
