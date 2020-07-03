732b020ea5a9064d0b28eb113a2a962c16893f9d
fibers <yu8582@126.com>
2020-07-03 14:14:56 +0800

    update

---
+++ src/main/java/com/fibers/algorithm/leetcode/_37/Solution.java
@@ -0,0 +1,15 @@
+package com.fibers.algorithm.leetcode._37;
+/*
+ * @lc app=leetcode.cn id=37 lang=java
+ *
+ * [37] 解数独
+ */
+
+// @lc code=start
+class Solution {
+    public void solveSudoku(char[][] board) {
+
+    }
+}
+// @lc code=end
+
